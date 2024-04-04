68986d5705a1099fa73f33803ac8d92a50cca9ef
Jean Parpaillon <jean.parpaillon@free.fr>
2024-04-04 19:29:57 +0200

    Remove dead code

--- priv/example_mnesia_migrations/create_binary_id_table.exs
+++
@@ -1,6 +0,0 @@
-IO.inspect :mnesia.create_table(:binary_id_table, [
-  ram_copies: [[:nonode@nohost]],
-  record_name: BinaryIdTestSchema,
-  attributes: [[:id, :inserted_at, :updated_at, :field]],
-  type: :set
-])
--- priv/migration_template.exs
+++
@@ -1,6 +0,0 @@
-IO.inspect :mnesia.create_table(TABLE_NAME, [
-  ram_copies: [NODES],
-  record_name: SCHEMA_NAME,
-  attributes: [FIELDS],
-  type: TABLE_TYPE
-])
